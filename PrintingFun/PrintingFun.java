abcdff6155d7aad37f90c41c98ed2496d83975d1
Roger Ngo <urbanspr1nter@gmail.com>
2022-09-26 11:25:44 -0700

    Examples from first lesson

---
+++ PrintingFun/PrintingFun.java
@@ -0,0 +1,12 @@
+public class PrintingFun {
+	public static void main(String[] args) {
+		System.out.println(1 + 1);
+		// 23
+		System.out.println("2" + 3);
+		// 2 3
+		System.out.println("2 " + 3);
+		System.out.println("2" + " " + 3);
+		System.out.println(1 + "3" + "3" + 7);
+		System.out.println(1 + 3 + "3" + "7");
+	}
+}
